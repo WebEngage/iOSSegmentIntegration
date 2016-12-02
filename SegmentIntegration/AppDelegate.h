ab02812cc43954b9eff79ede4152f417dd374ec6
Arpit Agrawal <meetmickeymoon@gmail.com>
2016-12-02 15:43:19 +0530

    Initial Commit

---
+++ SegmentIntegration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SegmentIntegration
+//
+//  Created by Yogesh on 02/12/16.
+//  Copyright © 2016 Arpit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SegmentIntegration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SegmentIntegration
+//
+//  Created by Yogesh on 02/12/16.
+//  Copyright © 2016 Arpit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
